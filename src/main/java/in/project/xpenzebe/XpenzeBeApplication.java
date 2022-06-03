aba5cf0ce1b104070447eba11aa5cf5ed321006c
MOHAN BABU S R <83107890+mohan-drive@users.noreply.github.com>
2022-06-03 10:14:43 +0530

    xpenze-be initial project setup
    Project with fresh lines of code, created spring boot project using Spring Initializr  (https://start.spring.io/ ) with 4 Dependencies ( Spring web, Lombok, Spring Boot DevTools, Spring Data MongoDB

---
+++ src/main/java/in/project/xpenzebe/XpenzeBeApplication.java
@@ -0,0 +1,13 @@
+package in.project.xpenzebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpenzeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpenzeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/project/xpenzebe/XpenzeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.project.xpenzebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpenzeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
